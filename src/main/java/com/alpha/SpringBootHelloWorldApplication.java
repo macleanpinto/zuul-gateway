489645c911bb60673a4d0a17e369a0a897162262
Maclean Pinto <Maclean.Pinto@gfs.com>
2018-07-12 13:45:44 +0530

    Initial Status

---
+++ src/main/java/com/alpha/SpringBootHelloWorldApplication.java
@@ -0,0 +1,42 @@
+package com.alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.alpha.filter.ErrorFilter;
+import com.alpha.filter.PostFilter;
+import com.alpha.filter.PreFilter;
+import com.alpha.filter.RouteFilter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class SpringBootHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+}
